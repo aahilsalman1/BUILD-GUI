427c3d2a7fe2bb29c21528f9c95e8b2baf06cc5d
Aahil Salman <aahil.salman1@icloud.com>
2026-03-11 17:45:07 +0000

    Add files via upload

--- audio to speech.py
+++ audio to speech.py
@@ -1,16 +1,63 @@
 from tkinter import *
 import speech_recognition as sr
+from tkinter import messagebox
 window = Tk()
-window.geometry("720x420")
-window.title("Speech_notepad")
+window.title("Speech to Text")
+window.geometry("1000x450")
+window.config(bg = "light grey")
 
-top_frame = Frame(window, bg = "pink", width = 725, height = 125)
-top_frame.place(x = 0, y = 0)
+text = ""
 
-title_label = Label(top_frame, text = "Voice Notepad", bg = "pink", font = ("Arial", 28))
-title_label.place(x = 230, y = 50)
+def recognise_speech():
+    global text
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak something...")
+        audio = r.listen(source)
+        
+    try:
+        text = text+" "+r.recognize_google(audio)
+        print(f"You said: {text}")
+        text_box.insert(END,text)
+    
+    except Exception as e:
+        print(f"Error: {e}")
 
-click_button = Button(window, text = "Click me to begin recording", bg = "yellow", font = ("Arial", 14),anchor = "s", width = 15, height = 3)
-click_button.place(x = 20, y = 150)
+def save_txt():
+    global text
+    file = open("output.txt","a")
+    file.write(text)
+    file.close()
+    messagebox.showinfo("Info","Your text is saved!")
+    
+
+title = Label(window,
+              text = "Voice Notepad",
+              font = ("Arial", 32),
+              bg = "light grey")
+title.place(x = 310, y = 30)
+
+record_btn = Button(window,
+                    text = "Click on me!..\nTo start recording",
+                    font = ("Arial", 14),
+                    width = 20,
+                    height = 3,
+                    bg = "light grey",command = recognise_speech)
+record_btn.place(x = 90, y = 180)
+
+text_box = Text(window,
+                width = 38,
+                height = 3,
+                font = ("Arial", 12))
+text_box.place(x = 360, y = 190)
+
+save_btn = Button(window,
+                  text = "Save the Text",
+                  font = ("Arial", 12),
+                  width = 16,
+                  height = 3,
+                  bg = "light grey", command = save_txt)
+save_btn.place(x = 720, y = 180)
 
 window.mainloop()
+
